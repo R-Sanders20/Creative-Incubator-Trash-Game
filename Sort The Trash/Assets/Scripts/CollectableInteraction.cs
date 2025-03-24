a710281d2668f650ab1ce301e145307d4af49621
ThomasH03 <115984651+ThomasH03@users.noreply.github.com>
2025-03-24 16:38:15 +0000

    Collectable Additions
    -Added the core code for the other bins
    -Added the core code for the other collectables
    -Added some commenting to the code that makes it clearer which part you are looking at (collision wise)

--- Sort The Trash/Assets/Scripts/CollectableInteraction.cs
+++ Sort The Trash/Assets/Scripts/CollectableInteraction.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CollectableInteraction : MonoBehaviour
 {
-    int blackCollectableStored = 0;
+    int blackCollectableStored,blueCollectableStored,greenCollectableStored  = 0;
     public SpawnCollectable spawnCollectableScript; // Reference to SpawnCollectable script
     public bool collected;
 
@@ -12,10 +12,14 @@ public class CollectableInteraction : MonoBehaviour
     {
         Debug.Log("Entered Trigger with: " + other.tag);
         
+        //Water
+
         if (other.CompareTag("Water"))
         {
                 transform.position = new Vector3(50.12734f, -0.841f, 1.243f);
         }
+
+        //Collectables
 
         if (other.CompareTag("Black Collectable") && collected == false)
         {
@@ -24,6 +28,30 @@ public class CollectableInteraction : MonoBehaviour
             Destroy(other.gameObject);
             collected = true;
         }
+        
+        
+        if (other.CompareTag("Blue Collectable") && collected == false)
+        {
+            blueCollectableStored++;
+            Debug.Log("Collectables:" + blueCollectableStored);
+            Destroy(other.gameObject);
+            collected = true;
+        }
+
+
+
+
+        if (other.CompareTag("Green Collectable") && collected == false)
+        {
+            greenCollectableStored++;
+            Debug.Log("Collectables:" + greenCollectableStored);
+            Destroy(other.gameObject);
+            collected = true;
+        }
+
+        
+
+        //Bins 
 
         if (other.CompareTag("Black Bin"))
         {
@@ -39,5 +67,37 @@ public class CollectableInteraction : MonoBehaviour
                 Debug.Log("no trash");
             }
         }
+
+        if (other.CompareTag("Blue Bin"))
+        {
+            if (blueCollectableStored > 0)
+            {
+                blackCollectableStored--;
+                Debug.Log("Blue Collectables now at: " + blueCollectableStored);
+                spawnCollectableScript.Deposited();
+                collected = false;
+            }
+            else
+            {
+                Debug.Log("no trash");
+            }
+        }
+
+        if (other.CompareTag("Green Bin"))
+        {
+            if (greenCollectableStored> 0)
+            {
+                greenCollectableStored--;
+                Debug.Log("Green Collectables now at: " + greenCollectableStored);
+                spawnCollectableScript.Deposited();
+                collected = false;
+            }
+            else
+            {
+                Debug.Log("no trash");
+            }
+        }
+
+
     }
 }
